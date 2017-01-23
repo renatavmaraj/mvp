ab19c943a71db218f0aa18608125b4c3655f9d2b
Renata Maraj <renatavmaraj@gmail.com>
2017-01-23 13:46:00 -0800

    Add config file and style page

---
+++ src/searchbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+var Search = () => {
+  return (
+    <div className="searchStyle">
+      <input  type="text" />
+      <button className="searchButton">Go!</button>
+    </div>
+    )
+  };
+
+
+export default Search;
+
